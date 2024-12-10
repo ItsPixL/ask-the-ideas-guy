e5adcd33782cad18bbe5293d698dbc5b4d332f91
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2024-12-10 22:01:53 +1100

    Fixed Player Controller WIP script after testing

--- Assets/Player_Controller_WIP.cs
+++ Assets/Player_Controller_WIP.cs
@@ -19,20 +19,20 @@ public class Player_Controller_WIP : MonoBehaviour
     void InitPlayerMovement() {
         // Allows character movement by player input.
         if (Input.GetKey("w")) { // forwards
-            MovePlayer(0f, 0f, force * Time.deltaTime);
+            MovePlayer(0f, 0f, force);
         }
         if (Input.GetKey("s")) { // backwards
-            MovePlayer(0f, 0f, -force * Time.deltaTime);
+            MovePlayer(0f, 0f, -force);
         }
         if (Input.GetKey("a")) { // left
-            MovePlayer(0f, -force * Time.deltaTime, 0f);
+            MovePlayer(-force, 0f, 0f);
         }
         if (Input.GetKey("d")) { // right
-            MovePlayer(0f, force * Time.deltaTime, 0f);
+            MovePlayer(force, 0f, 0f);
         }
     }
 
-    void Update() {
+    void FixedUpdate() {
         if (allowPlayerInput) {
             // All player input functions should be put here.
             InitPlayerMovement();
