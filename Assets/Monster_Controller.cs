6bba1aa03a20daae8d7960f80495578cac221c0e
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2024-12-15 13:48:09 +1100

    QoL Changes Added to Script
    Extra comments for clarity and reminders are provided, and the initialisation of the object is now less hard-coded.

--- Assets/Monster_Controller.cs
+++ Assets/Monster_Controller.cs
@@ -2,21 +2,31 @@ using UnityEngine;
 using System;
 using Unity.VisualScripting;
 
-public class Monster {
-    private float movementSpeed = 5f;
-    public int sightRange = 10;
-    private int loseSightRange = 15;
-    public GameObject monster;
+public class Monster { 
+    private float movementSpeed;
+    public int sightRange = 10; // Change this variable to private once OnDrawGizmos() is no longer needed.
+    private int loseSightRange;
+    public GameObject monster; // Change this variable to private once OnDrawGizmos() is no longer needed.
     private GameObject player;
     private bool seenPlayer = false;
-    public int fieldOfView = 160;
+    public int fieldOfView; // Change this variable to private once OnDrawGizmos() is no longer needed.
+
+    public Monster(float movementSpeed, int sightRange, int loseSightRange, int fieldOfView) {
+        // Constructor function for initialisation.
+        this.movementSpeed = movementSpeed;
+        this.sightRange = sightRange;
+        this.loseSightRange = loseSightRange;
+        this.fieldOfView = fieldOfView;
+    }
 
     public void initGameObjects(GameObject monster, GameObject player) {
+        // Passes all GameObjects to this class.
         this.monster = monster;
         this.player = player;
     }
 
     bool checkFOV(Vector2 playerPos, Vector2 monsterPos) {
+        // Checks whether the player is within the monster's field of view.
         Vector2 directionToPlayer = (playerPos-monsterPos).normalized;
         Vector2 monsterForward2D = new Vector2(monster.transform.forward.x, monster.transform.forward.z).normalized;
 
@@ -26,11 +36,13 @@ public class Monster {
     }
 
     bool reachableDistance(Vector2 playerPos, Vector2 monsterPos, int targetDistance) {
+        // Checks whether the player is close enough to the monster.
         float distance = Vector2.Distance(playerPos, monsterPos);
         return distance <= targetDistance;
     }
 
     bool detectedPlayer(int targetDistance) {
+        // Returns whether the player can be seen or not. 
         Vector2 playerPos2D = new Vector2(player.transform.position.x, player.transform.position.z);
         Vector2 monsterPos2D = new Vector2(monster.transform.position.x, monster.transform.position.z);
         if (reachableDistance(playerPos2D, monsterPos2D, sightRange) && checkFOV(playerPos2D, monsterPos2D)) {
@@ -46,12 +58,13 @@ public class Monster {
         else if (!detectedPlayer(sightRange)) {
             seenPlayer = false;
         }
-        // Debug.Log(seenPlayer); <-- Uncomment this line when testing.
+        // Uncomment the line below when testing.
+        // Debug.Log(seenPlayer);
     }
       
 }
 public class Monster_Controller : MonoBehaviour {
-    Monster entity = new Monster();
+    Monster entity = new Monster(5f, 10, 15, 160); 
 
     // The function below is for testing purposes only. It will be removed when all of the code is finalised.
     void OnDrawGizmos() {
@@ -66,6 +79,7 @@ public class Monster_Controller : MonoBehaviour {
         Gizmos.DrawRay(entity.monster.transform.position, new Vector3(leftBoundary.x, 0, leftBoundary.y) * targetDistance);
         Gizmos.DrawRay(entity.monster.transform.position, new Vector3(rightBoundary.x, 0, rightBoundary.y) * targetDistance);
     }
+
     void Start() {
         entity.initGameObjects(this.gameObject, GameObject.FindWithTag("Player"));
     }
