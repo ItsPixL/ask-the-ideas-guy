2b897fe59b056824ee7e9f50091c955be5c339e7
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2024-12-16 17:25:25 +1100

    Deleted the outdated player controller script and renamed the up-to-date script.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,21 +1,56 @@
 using UnityEngine;
+using InventoryManager;
 
-public class Player_Controller : MonoBehaviour {
+public class Player_Controller_WIP : MonoBehaviour {
     public Rigidbody rb;
-    public float force = 100f;
+    public float force = 5f;
+    private bool allowPlayerInput = true;
+    private Inventory playerInventory = new Inventory(3);
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        playerInventory.resetInventory();
+    }
+
+    void MovePlayer(float forceX, float forceY, float forceZ) {
+        // Moves the character.
+        rb.AddForce(forceX * Time.deltaTime, forceY * Time.deltaTime, forceZ * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    void InitPlayerMovement() {
+        // Allows character movement by player input.
+        if (Input.GetKey("w")) { // forwardss
+            MovePlayer(0f, 0f, force);
+        }
+        if (Input.GetKey("s")) { // backwards
+            MovePlayer(0f, 0f, -force);
+        }
+        if (Input.GetKey("a")) { // left
+            MovePlayer(-force, 0f, 0f);
+        }
+        if (Input.GetKey("d")) { // right
+            MovePlayer(force, 0f, 0f);
+        }
+    }
+
+    // Updates inventory information to respond to UI interaction.
+    void updateInventoryStatus(int targetIdx) {
+        playerInventory.fetchCurrItem(targetIdx);
+    }
+
+    // Update function used for all physics updates.
     void FixedUpdate() {
-        if (Input.GetKey("w")) { // forwards
-            rb.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+        if (allowPlayerInput) {
+            // All physics related player input functions should be put here.
+            InitPlayerMovement();
         }
-        else if (Input.GetKey("s")) { // backwards
-            rb.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey("a")) { // left
-            rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey("d")) { // right
-            rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    // All other updates are in the standard Update() function.
+    void Update() {
+        if (allowPlayerInput) {
+            // All other player input functions should be put here.
+            playerInventory.navigateInventory();
         }
     }
 }
