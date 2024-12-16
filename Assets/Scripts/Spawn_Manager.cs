2f226f80caaa9899faf62b629144a4b3ad16bb8c
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2024-12-16 16:27:25 +1100

    Initialised manager scripts.

---
+++ Assets/Scripts/Spawn_Manager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spawn_Manager : MonoBehaviour {
+    
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    
+}
---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour {
+    
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    
+}
